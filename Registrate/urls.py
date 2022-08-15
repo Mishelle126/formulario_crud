9c46f3bf0aa8f064d6795cb3e5b7a00ccd453239
Mishelle126 <97122505+Mishelle126@users.noreply.github.com>
2022-08-15 12:17:16 -0500

    update

--- Registrate/urls.py
+++ Registrate/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import login, create_nombre, delete_dato
+from .views import login, create_nombre, delete_dato, editar_dato
 urlpatterns =  [
     path('', login),
     path('new_nombre/', create_nombre, name='create_nombre'),
-    path('delete_dato/<int:Nombre_id>/', delete_dato, name='delete_dato')
+    path('delete_dato/<int:Nombre_id>/', delete_dato, name='delete_dato'),
+    path('editar_dato/<int:Nombre_id>/', editar_dato, name='editar_dato')
 ]
--- Registrate/views.py
+++ Registrate/views.py
@@ -14,3 +14,16 @@ def delete_dato(request, Nombre_id):
     Nombre = nombre.objects.get(id=Nombre_id)
     Nombre.delete()
     return redirect('/Registrate/')
+
+def editar_dato(request, Nombre_id):
+    Nombre = nombre.objects.get(id = Nombre_id)
+    if request.method == "POST":
+        
+        Nombre.nombre   = request.POST['nombre']
+        Nombre.apellido = request.POST['apellido']
+        Nombre.ciudad   = request.POST['ciudad']
+        Nombre.save()
+        return redirect('/Registrate/')
+        
+    return render(request, 'editar.html', {"Nombre": Nombre })
+ 
